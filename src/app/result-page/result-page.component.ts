fc8e657a3702d5c90425834871b71c8448bd3763
Vianney Accart <vianneyaccart@gmail.com>
2021-10-27 13:14:13 +0200

    Finalization of result page component with TypeScript new filters mobile menu. Karim's and Adam's components not included. Need to add Pole Emploi logo on desktop

--- src/app/result-page/result-page.component.ts
+++ src/app/result-page/result-page.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-result-page',
   templateUrl: './result-page.component.html',
-  styleUrls: ['./result-page.component.css']
+  styleUrls: ['./result-page.component.css'],
 })
 export class ResultPageComponent implements OnInit {
+  openedNewSearchMenu;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.openedNewSearchMenu = false;
   }
 
+  ngOnInit(): void {}
+
+  // Permet d'afficher/masquer le volet de nouvelle recherche sur mobile
+  openNewSearchMenu() {
+    this.openedNewSearchMenu = !this.openedNewSearchMenu;
+  }
 }
