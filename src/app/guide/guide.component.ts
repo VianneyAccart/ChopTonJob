247dd57657d364824f135a49c8ccdbf5b4725b28
Brice-Ducroux <brice-ducroux_student2021@wilder.school>
2021-10-29 14:10:19 +0200

    etape arrays in component.ts is almost done

--- src/app/guide/guide.component.ts
+++ src/app/guide/guide.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { cardsGuide } from '../shared/models/cardsGuide.model';
 
 @Component({
   selector: 'app-guide',
   templateUrl: './guide.component.html',
-  styleUrls: ['./guide.component.css']
+  styleUrls: ['./guide.component.css'],
 })
 export class GuideComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  etapes: cardsGuide[];
+  constructor() {
+    this.etapes = new Array();
   }
+  //generate cardsGuide and push them in the array 'etapes'
+  ngOnInit(): void {
+    const Etape1 = new cardsGuide(
+      'Etape1',
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      'assets/img/icons/calendarDateFill.svg',
+      'Icone calendrier'
+    );
+    const Etape2 = new cardsGuide(
+      'Etape2',
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      'assets/img/icons/github.svg',
+      'assets/img/icons/linkedin.svg'
+    );
+    const Etape3 = new cardsGuide(
+      'Etape3',
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      'assets/img/icons/fileText.svg',
+      'Icone fichier'
+    );
+    const Etape4 = new cardsGuide(
+      'Etape4',
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.'
+    );
 
+    this.etapes.push(Etape1, Etape2, Etape3, Etape4);
+  }
 }
---
+++ src/app/shared/models/cardsGuide.model.ts
@@ -0,0 +1,18 @@
+export class cardsGuide {
+  titre: string;
+  description: string;
+  imgSrc: string;
+  imgAlt: string;
+
+  constructor(
+    titre: string,
+    description: string,
+    imgSrc: string,
+    imgAlt: string
+  ) {
+    this.titre = titre;
+    this.description = description;
+    this.imgSrc = imgSrc;
+    this.imgAlt = imgAlt;
+  }
+}
