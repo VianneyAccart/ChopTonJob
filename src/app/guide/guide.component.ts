0d32c51ef1f0f02b3937a271a40df9654829d8cf
camille <camfrelat@gmail.com>
2021-11-03 12:28:53 +0100

    refactoring css code with Bootstrap for guide page

--- src/app/guide/guide.component.ts
+++ src/app/guide/guide.component.ts
@@ -13,31 +13,31 @@ export class GuideComponent implements OnInit {
   }
   //generate cardsGuide and push them in the array 'etapes'
   ngOnInit(): void {
-    const etape1 = new cardsGuide(
-      'Etape 1',
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-      'assets/img/icons/calendarDateFill.svg',
-      'Icone calendrier'
+    this.etapes.push(
+      new cardsGuide(
+        'Etape 1',
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        'assets/img/icons/calendarDateFill.svg',
+        'Icone calendrier'
+      ),
+      new cardsGuide(
+        'Etape 2',
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        'assets/img/icons/github.svg',
+        'Icone Github'
+      ),
+      new cardsGuide(
+        'Etape 3',
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        'assets/img/icons/linkedin.svg',
+        'Icone linkedin'
+      ),
+      new cardsGuide(
+        'Etape 4',
+        'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        'assets/img/icons/fileText.svg',
+        'Icone fichier'
+      )
     );
-    const etape2 = new cardsGuide(
-      'Etape 2',
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-      'assets/img/icons/github.svg',
-      'Icone Github'
-    );
-    const etape3 = new cardsGuide(
-      'Etape 3',
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-      'assets/img/icons/linkedin.svg',
-      'Icone linkedin'
-    );
-    const etape4 = new cardsGuide(
-      'Etape 4',
-      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
-      'assets/img/icons/fileText.svg',
-      'Icone fichier'
-    );
-
-    this.etapes.push(etape1, etape2, etape3, etape4);
   }
 }
