1e86d5dc84a89d46d1b390a6256be8d76bca50e8
camille <camfrelat@gmail.com>
2021-11-09 14:05:17 +0100

    unfinished regex for validator

--- src/app/shared/validators/emailPattern.validators.ts
+++ src/app/shared/validators/emailPattern.validators.ts
@@ -1,11 +1,11 @@
 import {AbstractControl, ValidationErrors} from '@angular/forms';
 
 export function emailPatternValidator(control: AbstractControl): ValidationErrors | null {
-    const referencePattern : RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
-    const valid = emailPatternValidator.test(control.value);
-    return valid
-      ? null
-      : {
-          pattern: 'Entrez une adresse email valide',
-        };
+  const emailPattern = RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+  const valid = emailPatternValidator(control.value);
+  return valid
+    ? null
+    : {
+        pattern: 'Entrez une adresse email valide',
+      };
 }
