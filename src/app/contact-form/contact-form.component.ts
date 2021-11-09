1a677e04bebd53200e5686e6b4f8cd5aa6d07387
camille <camfrelat@gmail.com>
2021-11-09 21:09:25 +0100

    finished validator email with Regex

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -13,7 +13,7 @@ export class ContactFormComponent {
     userFirstname: ['', [Validators.required, Validators.minLength(2)]],
     userLastname: ['', [Validators.required, Validators.minLength(2)]],
     userEmail: ['', [Validators.required, emailPatternValidator]],
-    userMessage: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(200)]],
+    userMessage: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(150)]],
   });
   constructor(private formBuilder: FormBuilder) {}
 
