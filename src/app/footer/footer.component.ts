e784fc7cdbbc180fdadddac92f05146cff90edae
AdamRoux <roux.adam69@gmail.com>
2021-11-19 10:25:47 +0100

    changed footer ngClass condition and correct contact page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -6,12 +6,24 @@ import {Router} from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   // Change navbar style according to url
   url = '';
   date = new Date().getFullYear();
+  isTransparent: boolean;
 
   constructor(private router: Router) {
     this.url = this.router.url;
+    this.isTransparent = false;
+  }
+
+  changeFooterBackground() {
+    if (this.url === '/' || this.url === '/404') {
+      this.isTransparent = true;
+    } else this.isTransparent = false;
+  }
+
+  ngOnInit() {
+    this.changeFooterBackground();
   }
 }
